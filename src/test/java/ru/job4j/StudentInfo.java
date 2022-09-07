ed056cadec73bab2be90500829aaf7a5f5f38544
Auto <yagudin.ravil1996@gmail.com>
2022-09-07 20:44:16 +0300

    3.2. Загрузка файлов на github [#243562]

--- src/test/java/ru/job4j/StudentInfo.java
+++ src/test/java/ru/job4j/StudentInfo.java
@@ -1,6 +1,7 @@
 package ru.job4j;
 
-public class StudentInfo {public static void main(String[] args) {
-    System.out.println("Auto Fujimoto");
-}
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Auto Fujimoto");
+    }
 }
